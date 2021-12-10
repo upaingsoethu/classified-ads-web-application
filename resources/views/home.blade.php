fdc3cbef94fea728d746477a3f5827f10e9e0e8c
paingsoethu <kopaingsoethu2015@gmail.com>
2021-12-10 13:33:08 +0630

    Route basic

---
+++ resources/views/home.blade.php
@@ -0,0 +1,1 @@
+<h1>Welcome Laravel Home </h1>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about-us','TestController@index')->name('about');
+
+
